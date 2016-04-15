3ea50158c3651f7cb26e1bc8f57b12f5c2ad0259
jza <jake@kirkandstanley.se>
2016-04-15 14:38:43 +0200

    Fixed error in gulp file.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('styles', function() {
+gulp.task('default', function() {
   gulp.src('src/sass/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./src/css/'));
 });
 
-gulp.watch('src/sass/*.scss', ['styles'], function(event) {
+gulp.watch('src/sass/*.scss', ['default'], function(event) {
   console.log(event.path + ' was ' + event.type);
 });
